befcc38239bf5283c43f2f9edebd99a338ecaae5
softrahuldevit <rahuldeokate35@gmail.com>
2025-08-20 23:35:19 +0530

    first ct

---
+++ .vscode/practice.js
@@ -0,0 +1,23 @@
+
+var d =30;
+var e = 40;
+var f=d+e;
+console.log(f);
+
+
+const result=function()
+{
+    return "Hello World";
+}
+console.log(result());
+
+console.log('-----------------------');
+const res =()=>{
+ return "I am Programmer";
+}
+console.log(res());
+
+function car( driver)
+{
+    return 
+}
